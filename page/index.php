958b572480ad459667d8aa31bbe65afc9a6aabe9
glzaboy <glzaboy@163.com>
2015-05-11 22:08:33 +0800

    add sql test
    add sql test

--- page/index.php
+++ page/index.php
@@ -8,8 +8,27 @@ class index extends \fl\base\page
 
     function dohome($pathcmd)
     {
-        phpinfo();      
-
+        $sqlBuilder = \fl\db\QueryBuilder::getQueryerBuilder(new \fl\db\connect('cp'));
+        $sqlBuilder->disableprefix();
+        $data = array();
+        $data['ua'] = $_SERVER['HTTP_USER_AGENT'];
+        $data['ip'] = $_SERVER['REMOTE_ADDR'];
+        $data['time'] = date('Y-m-d H:i:s');
+        $join = array();
+        $join['tabl e2'] = "fsdfdsfds";
+        $join['taba le3'] = array(
+            'right',
+            'id' => 'test',
+            'id2' => 'test2'
+        );
+        $condition=array();
+        $condition['name']="china";
+        $sqlBuilder->select('tab fs', $condition, '*', '', '', $join);
+        // $http = new \fl\helpers\httpclient();
+        // $data = json_decode($http->getData("http://hello.api.235dns.com/api.php?code=json"), true);
+        // $tplvar = array();
+        // $tplvar['words'] = $data['words'];
+        // return $this->render($tplvar);
     }
 
     function doconcat()

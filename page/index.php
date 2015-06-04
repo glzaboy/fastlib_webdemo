043cda628c8dde3b293ac876529649692a59022a
glzaboy <glzaboy@163.com>
2015-06-05 03:03:02 +0800

    add php file
    add php info

--- page/index.php
+++ page/index.php
@@ -6,39 +6,8 @@ class index extends \fl\base\page
 
     function dohome($pathcmd)
     {
-//         $sqlBuilder = \fl\db\QueryBuilder::getQueryerBuilder(new \fl\db\connect('cp'));
-// //         $sqlBuilder->disableprefix();
-//         $data = array();
-//         $data['ua'] = $_SERVER['HTTP_USER_AGENT'];
-//         $data['ip'] = $_SERVER['REMOTE_ADDR'];
-//         $data['time'] = date('Y-m-d H:i:s');
-//         $join = array();
-// //         $join['tabl e2'] = "fsdfdsfds";
-// //         $join['taba le3'] = array(
-// //             'right',
-// //             'id' => 'test',
-// //             'id2' => 'test2'
-// //         );
-//         $condition = array();
-//         $condition['name'] = "111对对对";
-//         try{
-//             $data2=$sqlBuilder->select('webconfig', $condition, '*', '', '', $join);
-//             print_r($data2);
-//             foreach ($data2 as $v){
-//                 print_r($v);
-//             }
-//             foreach ($data2 as $v){
-//                 print_r($v);
-//             }
-//         }catch (\Exception $e){
-//             print_r($e->getMessage());
-//         }
-//         print_r($data[0]);
-        // $http = new \fl\helpers\httpclient();
-        // $data = json_decode($http->getData("http://hello.api.235dns.com/api.php?code=json"), true);
-        // $tplvar = array();
-        // $tplvar['words'] = $data['words'];
-//         return $this->render($tplvar);
+        echo "php";
+        phpinfo();
     }
 
 }

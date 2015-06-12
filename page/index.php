1797e48a5f880663a355de27fb5a36d97a93fd41
glzaboy <glzaboy@163.com>
2015-06-12 18:30:42 +0800

    [publish] test composer

--- page/index.php
+++ page/index.php
@@ -8,7 +8,7 @@ class index extends \fl\base\page
 
     function dohome($pathcmd)
     {
-        echo date('Y-m-d').date('H:i:s'); exit;
+        echo date('Y-m-d').date('H:i:s'); 
 //         $l=new QueryBuilder(new connect('cp'));
 //         $l->select('cp', $orderby, $groupby);
     }

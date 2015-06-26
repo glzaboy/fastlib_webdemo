901e8a6fdf15947f630bc891cd9b80f52cbb1169
glzaboy <glzaboy@163.com>
2015-06-26 18:27:10 +0800

    [publish] add select
    [publish] add select

--- page/index.php
+++ page/index.php
@@ -3,15 +3,26 @@ namespace page;
 
 use fl\db\QueryBuilder;
 use fl\db\connect;
+use fl\db\transaction;
 class index extends \fl\base\page
 {
 
     function dohome($pathcmd)
     {
-        echo date('Y-m-d').date('H:i:s');
-        phpinfo();
-//         $l=new QueryBuilder(new connect('cp'));
-//         $l->select('cp', $orderby, $groupby);
+        echo date('Y-m-d H:i:s');
+        try{
+            $con=new connect('yiiiot');
+            $l=$con->getQueryerBuilder();
+            print_r($l->gettables());
+            $data=$l->select('user','','*','order by user_id desc');
+            $l->update('user',array('showname'=>"飞雪欢春"),array('user_id=3'));
+            $l->delete('user',array('user_id'=>2));
+            foreach ($data as $d){
+                print_r($d);
+            }
+        }catch (\Exception $e){
+            print_r($e);
+        }
     }
 
 }

1ff445e8d76c129c08693c36cbb8a810932a237c
glzaboy <glzaboy@163.com>
2015-06-29 01:52:12 +0800

    [publish] test query builder

--- page/index.php
+++ page/index.php
@@ -1,9 +1,7 @@
 <?php
 namespace page;
 
-use fl\db\QueryBuilder;
 use fl\db\connect;
-use fl\db\transaction;
 
 class index extends \fl\base\page
 {
@@ -14,22 +12,24 @@ class index extends \fl\base\page
             $con = new connect('yiiiot');
             $l = $con->getQueryerBuilder();
             $condition = array();
-            $condition['user.user_id'] = 1;
+            $condition['user.user_id'] = array(
+                1,
+                3
+            );
             $leftjoin = array();
             $leftjoin['usercfg'] = array(
                 'left',
                 'usercfg.user_id' => 'user.user_id'
             );
-            $item=array();
-            $item['id']='user.user_id';
-            $item['id2']='usercfg.user_id';
-
+            $item = array();
+            $item['id'] = 'user.user_id';
+            $item['id2'] = 'usercfg.user_id';
+            $item[] = "user.email";
             $data = $l->select('user', $condition, $item, 'order by yi_user.user_id desc', '', $leftjoin);
-            // $l->update('user',array('showname'=>"飞雪欢春"),array('user_id=3'));
-            // $l->delete('user',array('user_id'=>2));
             foreach ($data as $d) {
                 print_r($d);
             }
+            echo $data->debugDumpParams();
         } catch (\Exception $e) {
             print_r($e);
         }
